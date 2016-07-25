191d9cae4bd50fc8a2e7e20bd49c222a7fe4b20e
unpete <info@oknosoft.ru>
2016-07-25 17:44:10 +0500

    v0.0.1

--- src/modifiers/doc_buyers_order_form_obj.js
+++ src/modifiers/doc_buyers_order_form_obj.js
@@ -11,84 +11,12 @@ $p.modifiers.push(
 
 	function($p) {
 
-		var _mgr = $p.doc.buyers_order,
-			_meta_patched;
+		var _mgr = $p.doc.buyers_order;
 
 
 		_mgr.form_obj = function(pwnd, attr){
 
 			var o, wnd, evts = [], attr_on_close = attr.on_close;
-
-			/**
-			 * структура заголовков табчасти продукции
-			 * @param source
-			 */
-			if(!_meta_patched){
-				(function(source){
-					// TODO: штуки сейчас спрятаны в ro и имеют нулевую ширину
-					if($p.wsql.get_user_param("hide_price_dealer")){
-						source.headers = "№,Номенклатура,Характеристика,Комментарий,Штук,Длина,Высота,Площадь,Колич.,Ед,Скидка,Цена,Сумма,Скидка&nbsp;дил,Цена&nbsp;дил,Сумма&nbsp;дил";
-						source.widths = "40,200,*,220,0,70,70,70,70,40,70,70,70,0,0,0";
-						source.min_widths = "30,200,220,150,0,70,40,70,70,70,70,70,70,0,0,0";
-
-					}else if($p.wsql.get_user_param("hide_price_manufacturer")){
-						source.headers = "№,Номенклатура,Характеристика,Комментарий,Штук,Длина,Высота,Площадь,Колич.,Ед,Скидка&nbsp;пост,Цена&nbsp;пост,Сумма&nbsp;пост,Скидка,Цена,Сумма";
-						source.widths = "40,200,*,220,0,70,70,70,70,40,0,0,0,70,70,70";
-						source.min_widths = "30,200,220,150,0,70,40,70,70,70,0,0,0,70,70,70";
-
-					}else{
-						source.headers = "№,Номенклатура,Характеристика,Комментарий,Штук,Длина,Высота,Площадь,Колич.,Ед,Скидка&nbsp;пост,Цена&nbsp;пост,Сумма&nbsp;пост,Скидка&nbsp;дил,Цена&nbsp;дил,Сумма&nbsp;дил";
-						source.widths = "40,200,*,220,0,70,70,70,70,40,70,70,70,70,70,70";
-						source.min_widths = "30,200,220,150,0,70,40,70,70,70,70,70,70,70,70,70";
-					}
-
-					if($p.current_acl.role_available("СогласованиеРасчетовЗаказов") || $p.current_acl.role_available("РедактированиеСкидок"))
-						source.types = "cntr,ref,ref,txt,ro,calck,calck,calck,calck,ref,calck,calck,ro,calck,calck,ro";
-					else
-						source.types = "cntr,ref,ref,txt,ro,calck,calck,calck,calck,ref,ro,ro,ro,calck,calck,ro";
-
-				})($p.doc.buyers_order.metadata().form.obj.tabular_sections.production);
-				_meta_patched = true;
-			}
-
-			attr.draw_tabular_sections = function (o, wnd, tabular_init) {
-
-				/**
-				 * получим задействованные в заказе объекты характеристик
-				 */
-				var refs = [];
-				o.production.each(function (row) {
-					if(!$p.utils.is_empty_guid(row._obj.characteristic) && row.characteristic.is_new())
-						refs.push(row._obj.characteristic);
-				});
-				$p.cat.characteristics.pouch_load_array(refs)
-					.then(function () {
-
-						// табчасть продукции со специфическим набором кнопок
-						tabular_init("goods");
-
-						var toolbar = wnd.elmnts.tabs.tab_production.getAttachedToolbar();
-						toolbar.addSpacer("btn_delete");
-						toolbar.attachEvent("onclick", toolbar_click);
-
-						// табчасть планирования
-						tabular_init("planning");
-
-
-						// попап для присоединенных файлов
-						wnd.elmnts.discount_pop = new dhtmlXPopup({
-							toolbar: toolbar,
-							id: "btn_discount"
-						});
-						wnd.elmnts.discount_pop.attachEvent("onShow", show_discount);
-
-						// в зависимости от статуса
-						setTimeout(set_editable, 50);
-
-					});
-
-
-			};
 
 			attr.draw_pg_header = function (o, wnd) {
 
@@ -101,6 +29,9 @@ $p.modifiers.push(
 						}
 					}, 200);
 				}
+
+				// в зависимости от статуса
+				setTimeout(set_editable, 50);
 
 				/**
 				 *	закладка шапка
@@ -122,15 +53,13 @@ $p.modifiers.push(
 					read_only: wnd.elmnts.ro,
 					oxml: {
 						" ": [{id: "number_doc", path: "o.number_doc", synonym: "Номер", type: "ro", txt: o.number_doc},
-							{id: "date", path: "o.date", synonym: "Дата", type: "ro", txt: $p.moment(o.date).format($p.moment._masks.date_time)},
-							"number_internal"
+							{id: "date", path: "o.date", synonym: "Дата", type: "ro", txt: $p.moment(o.date).format($p.moment._masks.date_time)}
 							],
-						"Контактная информация": ["partner", "client_of_dealer", "phone",
-							{id: "shipping_address", path: "o.shipping_address", synonym: "Адрес доставки", type: "addr", txt: o["shipping_address"]}
+						"Контактная информация": ["partner",
+							{id: "shipping_address", path: "o.shipping_address", synonym: "Адрес доставки", type: "txt", txt: o["shipping_address"]}
 						],
 						"Дополнительные реквизиты": [
-							{id: "obj_delivery_state", path: "o.obj_delivery_state", synonym: "Состояние транспорта", type: "ro", txt: o["obj_delivery_state"].presentation},
-							"category"
+							{id: "obj_delivery_state", path: "o.obj_delivery_state", synonym: "Состояние транспорта", type: "ro", txt: o["obj_delivery_state"].presentation}
 						]
 					}
 				});
@@ -155,9 +84,10 @@ $p.modifiers.push(
 						"Аналитика": ["project",
 							{id: "organization", path: "o.organization", synonym: "Организация", type: "refc", txt: o["organization"].presentation},
 							"contract", "organizational_unit", "department"],
-						"Итоги": [{id: "doc_currency", path: "o.doc_currency", synonym: "Валюта документа", type: "ro", txt: o["doc_currency"].presentation},
-							{id: "doc_amount", path: "o.doc_amount", synonym: "Сумма", type: "ron", txt: o["doc_amount"]},
-							{id: "amount_internal", path: "o.amount_internal", synonym: "Сумма внутр", type: "ron", txt: o["amount_internal"]}]
+						"Итоги": [
+							{id: "doc_currency", path: "o.doc_currency", synonym: "Валюта документа", type: "ro", txt: o["doc_currency"].presentation},
+							{id: "doc_amount", path: "o.doc_amount", synonym: "Сумма", type: "ron", txt: o["doc_amount"]}
+							]
 					}
 				});
 
@@ -168,19 +98,7 @@ $p.modifiers.push(
 				wnd.elmnts.cell_note.hideHeader();
 				wnd.elmnts.cell_note.setHeight(100);
 				wnd.elmnts.cell_note.attachHTMLString("<textarea class='textarea_editor'>" + o.note + "</textarea>");
-				// wnd.elmnts.note_editor = wnd.elmnts.cell_note.attachEditor({
-				// 	content: o.note,
-				// 	onFocusChanged: function(name, ev){
-				// 		if(!wnd.elmnts.ro && name == "blur")
-				// 			o.note = this.getContent().replace(/&nbsp;/g, " ").replace(/<.*?>/g, "").replace(/&.{2,6};/g, "");
-				// 	}
-				// });
 
-				//wnd.elmnts.pg_header = wnd.elmnts.tabs.tab_header.attachHeadFields({
-				//	obj: o,
-				//	pwnd: wnd,
-				//	read_only: wnd.elmnts.ro    // TODO: учитывать права для каждой роли на каждый объект
-				//});
 			};
 
 			attr.toolbar_struct = $p.injected_data["toolbar_buyers_order_obj.xml"];
@@ -259,9 +177,6 @@ $p.modifiers.push(
 
 						break;
 
-					case 'btn_calendar':
-						calendar_new_event();
-						break;
 
 					case 'btn_go_connection':
 						go_connection();
@@ -272,12 +187,6 @@ $p.modifiers.push(
 					_mgr.print(o, btn_id, wnd);
 			}
 
-			/**
-			 * создаёт событие календаря
-			 */
-			function calendar_new_event(){
-				$p.msg.show_not_implemented();
-			}
 
 			/**
 			 * показывает список связанных документов
@@ -285,48 +194,6 @@ $p.modifiers.push(
 			function go_connection(){
 				$p.msg.show_not_implemented();
 			}
-
-			/**
-			 * создаёт и показывает диалог групповых скидок
-			 */
-			function show_discount(){
-				if (!wnd.elmnts.discount) {
-
-					wnd.elmnts.discount = wnd.elmnts.discount_pop.attachForm([
-						{type: "fieldset",  name: "discounts", label: "Скидки по группам", width:220, list:[
-							{type:"settings", position:"label-left", labelWidth:100, inputWidth:50},
-							{type:"input", label:"На продукцию", name:"production", numberFormat:["0.0 %", "", "."]},
-							{type:"input", label:"На аксессуары", name:"accessories", numberFormat:["0.0 %", "", "."]},
-							{type:"input", label:"На услуги", name:"services", numberFormat:["0.0 %", "", "."]}
-						]},
-						{ type:"button" , name:"btn_discounts", value:"Ок", tooltip:"Установить скидки"  }
-					]);
-					wnd.elmnts.discount.setItemValue("production", 0);
-					wnd.elmnts.discount.setItemValue("accessories", 0);
-					wnd.elmnts.discount.setItemValue("services", 0);
-					wnd.elmnts.discount.attachEvent("onButtonClick", function(name){
-						wnd.progressOn();
-						// TODO: _mgr.save
-						//_mgr.save({
-						//	ref: o.ref,
-						//	discounts: {
-						//		production: $p.utils.fix_number(wnd.elmnts.discount.getItemValue("production"), true),
-						//		accessories: $p.utils.fix_number(wnd.elmnts.discount.getItemValue("accessories"), true),
-						//		services: $p.utils.fix_number(wnd.elmnts.discount.getItemValue("services"), true)
-						//	},
-						//	o: o._obj,
-						//	action: "calc",
-						//	specify: "discounts"
-						//}).then(function(res){
-						//	if(!$p.msg.check_soap_result(res))
-						//		wnd.reflect_characteristic_change(res); // - перезаполнить шапку и табчасть
-						//	wnd.progressOff();
-						//	wnd.elmnts.discount_pop.hide();
-						//});
-					});
-				}
-			}
-
 
 			/**
 			 * обработчик выбора значения в таблице продукции (ссылочные типы)
@@ -402,103 +269,6 @@ $p.modifiers.push(
 			/**
 			 * вспомогательные функции
 			 */
-
-			/**
-			 * настройка (инициализация) табличной части продукции
-			 */
-			function production_init(){
-
-
-				// собственно табличная часть
-				var grid = wnd.elmnts.grids.production,
-					source = {
-						o: o,
-						wnd: wnd,
-						on_select: production_on_value_select,
-						tabular_section: "production",
-						footer_style: "text-align: right; font: bold 12px Tahoma; color: #005; background: #f9f9f9; height: 22px;"
-					};
-				production_captions(source);
-
-				grid.setIconsPath(dhtmlx.image_path);
-				grid.setImagePath(dhtmlx.image_path);
-
-				// 16 полей
-				//row, nom, characteristic, note, qty, len, width, s, quantity, unit, discount_percent, price, amount, discount_percent_internal, price_internal, amount_internal
-				grid.setHeader(source.headers);
-				grid.setInitWidths(source.widths);
-				grid.setColumnMinWidth(source.min_widths);
-
-				grid.setColumnIds(source.fields.join(","));
-				grid.enableAutoWidth(true, 1200, 600);
-				grid.enableEditTabOnly(true);
-
-				grid.init();
-				//grid.enableLightMouseNavigation(true);
-				//grid.enableKeyboardSupport(true);
-				//grid.splitAt(2);
-
-				grid.attachFooter("Итого:,#cspan,#cspan,#cspan,#cspan,#cspan,#cspan,#cspan,#cspan,#cspan,#cspan,#cspan,{#stat_total}, ,#cspan,{#stat_total}",
-					[source.footer_style, "","","","","","","","","","","",source.footer_style,source.footer_style,"",source.footer_style]);
-
-				grid.setUserData("", "source", source);
-				grid.attachEvent("onEditCell", production_on_edit);
-				grid.attachEvent("onRowSelect", production_on_row_activate);
-			}
-
-			function production_new_row(){
-				var row = o["production"].add({
-					qty: 1,
-					quantity: 1,
-					discount_percent_internal: $p.wsql.get_user_param("discount", "number")
-				});
-				o["production"].sync_grid(wnd.elmnts.grids.production);
-				wnd.elmnts.grids.production.selectRowById(row.row);
-				return row;
-			}
-
-			function production_get_sel_index(){
-				var selId = wnd.elmnts.grids.production.getSelectedRowId();
-				if(selId && !isNaN(Number(selId)))
-					return Number(selId)-1;
-
-				$p.msg.show_msg({
-					type: "alert-warning",
-					text: $p.msg.no_selected_row.replace("%1", "Продукция"),
-					title: o.presentation
-				});
-			}
-
-			function production_del_row(){
-
-				var rId = production_get_sel_index(), row;
-
-				if(rId == undefined)
-					return;
-				else
-					row = o["production"].get(rId);
-
-				// проверяем, не подчинена ли текущая строка продукции
-				if($p.utils.is_data_obj(row.ordn) && !row.ordn.empty()){
-					// возможно, ссылка оборвана. в этом случае, удаление надо разрешить
-					if(o["production"].find({characteristic: row.ordn})){
-						$p.msg.show_msg({
-							type: "alert-warning",
-							text: $p.msg.sub_row_change_disabled,
-							title: o.presentation + ' стр. №' + (rId + 1)
-						});
-						return;
-					}
-				}
-
-				// если удаляем строку продукции, за одно надо удалить и подчиненные аксессуары
-				if($p.utils.is_data_obj(row.characteristic) && !row.characteristic.empty()){
-					o["production"].find_rows({ordn: row.characteristic}).forEach(function (r) {
-						o["production"].del(r);
-					});
-				}
-
-			}
 
 			function save(action){
 
@@ -588,11 +358,11 @@ $p.modifiers.push(
 
 				// технолог может изменять шаблоны
 				if(o.obj_delivery_state == $p.enm.obj_delivery_states.Шаблон){
-					wnd.elmnts.ro = !$p.current_acl.role_available("ИзменениеТехнологическойНСИ");
+					wnd.elmnts.ro = !$p.current_acl.role_available("МенеджерПоПродажам");
 
 				// ведущий менеджер может изменять проведенные
 				}else if(o.posted || o._deleted){
-					wnd.elmnts.ro = !$p.current_acl.role_available("СогласованиеРасчетовЗаказов");
+					wnd.elmnts.ro = !$p.current_acl.role_available("МенеджерПоПродажам");
 
 				}else if(!wnd.elmnts.ro && !o.obj_delivery_state.empty())
 					wnd.elmnts.ro = o.obj_delivery_state != st_draft && o.obj_delivery_state != st_retrieve;
@@ -600,13 +370,12 @@ $p.modifiers.push(
 				retrieve_enabed = !o._deleted &&
 					(o.obj_delivery_state == $p.enm.obj_delivery_states.Отправлен || o.obj_delivery_state == $p.enm.obj_delivery_states.Отклонен);
 
-				wnd.elmnts.grids.production.setEditable(!wnd.elmnts.ro);
-				wnd.elmnts.grids.planning.setEditable(!wnd.elmnts.ro);
+				wnd.elmnts.grids.goods.setEditable(!wnd.elmnts.ro);
 				wnd.elmnts.pg_left.setEditable(!wnd.elmnts.ro);
 				wnd.elmnts.pg_right.setEditable(!wnd.elmnts.ro);
 
 				// гасим кнопки проведения, если недоступна роль
-				if(!$p.current_acl.role_available("СогласованиеРасчетовЗаказов")){
+				if(!$p.current_acl.role_available("МенеджерПоПродажам")){
 					wnd.elmnts.frm_toolbar.hideItem("btn_post");
 					wnd.elmnts.frm_toolbar.hideItem("btn_unpost");
 				}
@@ -616,12 +385,7 @@ $p.modifiers.push(
 					wnd.elmnts.frm_toolbar.disableItem("btn_sent");
 					wnd.elmnts.frm_toolbar.disableItem("btn_save");
 
-					detales_toolbar = wnd.elmnts.tabs.tab_production.getAttachedToolbar();
-					detales_toolbar.forEachItem(function(itemId){
-						detales_toolbar.disableItem(itemId);
-					});
-
-					detales_toolbar = wnd.elmnts.tabs.tab_planning.getAttachedToolbar();
+					detales_toolbar = wnd.elmnts.tabs.tab_goods.getAttachedToolbar();
 					detales_toolbar.forEachItem(function(itemId){
 						detales_toolbar.disableItem(itemId);
 					});
@@ -635,12 +399,7 @@ $p.modifiers.push(
 
 					wnd.elmnts.frm_toolbar.enableItem("btn_save");
 
-					detales_toolbar = wnd.elmnts.tabs.tab_production.getAttachedToolbar();
-					detales_toolbar.forEachItem(function(itemId){
-						detales_toolbar.enableItem(itemId);
-					});
-
-					detales_toolbar = wnd.elmnts.tabs.tab_planning.getAttachedToolbar();
+					detales_toolbar = wnd.elmnts.tabs.tab_goods.getAttachedToolbar();
 					detales_toolbar.forEachItem(function(itemId){
 						detales_toolbar.enableItem(itemId);
 					});
